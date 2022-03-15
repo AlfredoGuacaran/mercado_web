d6dab92f188cc0dc3dfbaac6f8a414c83d46b9b6
AlfredoGuacaran <alfredoguacaran21@gmail.com>
2022-03-15 18:55:34 -0300

    carrito %

--- server.js
+++ server.js
@@ -4,15 +4,18 @@ const app = express();
 
 app.use(express.static('static'));
 app.use(express.static('node_modules/bootstrap/dist'));
+app.use(express.static('node_modules/jquery/dist/jquery.js'));
 
 const productos = [
-  { nombre: 'Banana', img: 'banana.png', precio: 990 },
-  { nombre: 'Cebolla', img: 'cebolla.png', precio: 1200 },
-  { nombre: 'Lechuga', img: 'lechuga.png', precio: 1300 },
-  { nombre: 'Papa', img: 'papa.png', precio: 1350 },
-  { nombre: 'Piminton', img: 'pimenton.png', precio: 1990 },
-  { nombre: 'Tomate', img: 'tomate.png', precio: 1800 },
+  { id: 1, nombre: 'Banana', img: 'banana.png', precio: 990 },
+  { id: 2, nombre: 'Cebolla', img: 'cebolla.png', precio: 1200 },
+  { id: 3, nombre: 'Lechuga', img: 'lechuga.png', precio: 1300 },
+  { id: 4, nombre: 'Papa', img: 'papa.png', precio: 1350 },
+  { id: 5, nombre: 'Piminton', img: 'pimenton.png', precio: 1990 },
+  { id: 6, nombre: 'Tomate', img: 'tomate.png', precio: 1800 },
 ];
+
+let carrito = [];
 
 nunjucks.configure('views', {
   express: app,
@@ -21,7 +24,33 @@ nunjucks.configure('views', {
 });
 
 app.get('/', (req, res) => {
-  res.render('index.html', { productos });
+  res.render('index.html', { productos, carrito });
+});
+
+app.post('/agregar', async (req, res) => {
+  let data;
+  req.on('data', payload => {
+    data = JSON.parse(payload);
+  });
+
+  req.on('end', async () => {
+    try {
+      const { productoid } = data;
+
+      const productInf = productos.filter(
+        producto => producto.id == productoid
+      )[0];
+
+      carrito.push(productInf);
+
+      console.log(carrito);
+
+      res.status(200).send({ Mensaje: 'Exitoso' });
+    } catch (error) {
+      console.log(error);
+      res.status(400).send({ error });
+    }
+  });
 });
 
 app.listen(3000, () => console.log('Servidor en puerto 3000'));
